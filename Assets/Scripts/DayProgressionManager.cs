2a961181cc18f22f7c799fb40055ce504ed86138
Ager <aghazofficial@gmail.com>
2026-08-18 06:50:37 +0800

    Finished the line trace mechanic

--- Assets/Scripts/DayProgressionManager.cs
+++ Assets/Scripts/DayProgressionManager.cs
@@ -11,32 +11,39 @@ public class DayProgressionManager : MonoBehaviour
 
     private int currentDayIndex = 0;
     private int currentScenarioIndex = 0;
+
     public OptionNode[] choiceNodes;
 
     void Start()
     {
         currentDayIndex = PlayerPrefs.GetInt("SavedDay", 0);
-
         if (currentDayIndex >= allDays.Length)
         {
-            currentDayIndex = 0;
+            TriggerEmergency();
         }
-
-        DisplayCurrentScenario();
+        else
+        {
+            DisplayCurrentScenario();
+        }
     }
 
     public void DisplayCurrentScenario()
     {
+        scenarioDisplayText.color = Color.white;
+
         DayData today = allDays[currentDayIndex];
         ScenarioData currentScenario = today.scenarios[currentScenarioIndex];
 
         scenarioDisplayText.text = "Day " + (currentDayIndex + 1) + "\n\n" + currentScenario.scenarioDescription;
+
         for (int i = 0; i < 3; i++)
         {
+            choiceNodes[i].gameObject.SetActive(true);
             choiceNodes[i].SetupNode(currentScenario.options[i]);
             choiceNodes[i].ResetTrace();
         }
     }
+
     public void OnTraceCompleted()
     {
         currentScenarioIndex++;
@@ -50,20 +57,59 @@ public class DayProgressionManager : MonoBehaviour
             DisplayCurrentScenario();
         }
     }
+
     private void CompleteDay()
     {
         currentDayIndex++;
         currentScenarioIndex = 0;
+
         PlayerPrefs.SetInt("SavedDay", currentDayIndex);
         PlayerPrefs.Save();
 
         if (currentDayIndex >= allDays.Length)
         {
-            scenarioDisplayText.text = "Week Complete!\n\nTriggering the Emergency Protocol.";
+            TriggerEmergency();
         }
         else
         {
             DisplayCurrentScenario();
+        }
+    }
+
+    private void TriggerEmergency()
+    {
+        foreach (OptionNode node in choiceNodes)
+        {
+            if (node != null) node.gameObject.SetActive(false);
+        }
+        float finalSavings = PlayerPrefs.GetFloat("MonthlyStipend", 0f);
+        float emergencyCost = 80.00f; 
+        if (finalSavings >= emergencyCost)
+        {
+            float remaining = finalSavings - emergencyCost;
+            float bonus = remaining * 0.02f; 
+
+            scenarioDisplayText.text =
+                "DAY 6: EMERGENCY!\nYour school laptop screen shattered. Repair Cost: $80.00\n\n" +
+                "RESULT: Your savings absorbed the hit.\n" +
+                "Remaining Vault Balance: $" + remaining.ToString("F2") + "\n" +
+                "DBS PayLah Bonus Interest: +$" + bonus.ToString("F2") + "\n\n" +
+                "FINANCIALLY SECURE";
+
+            scenarioDisplayText.color = Color.green;
+        }
+        else
+        {
+            float debt = emergencyCost - finalSavings;
+
+            scenarioDisplayText.text =
+                "DAY 6: EMERGENCY!\nYour school laptop screen shattered. Repair Cost: $80.00\n\n" +
+                "RESULT: You only have $" + finalSavings.ToString("F2") + " saved.\n" +
+                "Shortfall: $" + debt.ToString("F2") + "\n\n" +
+                "This shortfall becomes a high interest loan. You are now trapped in the poverty cycle.\n\n" +
+                "IN DEBT";
+
+            scenarioDisplayText.color = Color.red;
         }
     }
 }
